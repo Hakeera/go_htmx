34b61b28618b74c8b977efbd75121b5f2ac816c6
Hakeera <jxhcbhudgfv@gmail.com>
2025-03-07 10:32:07 -0300

    Acertando Routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,12 +1,24 @@
 package routes
 
 import (
+	"github.com/Hakeera/crud/controller"
 	"github.com/gin-gonic/gin"
 )
 
 // SetupRoutes configura todas as rotas do servidor
 func SetupRoutes() *gin.Engine {
 	r := gin.Default()
+
+	// Servir arquivos estáticos (se necessário)
+	r.Static("/static", "./static")
+
+	// Carregar templates HTML
+	r.LoadHTMLGlob("view/*.html")
+
+	// Rota para a página inicial
+	r.GET("/", func(c *gin.Context) {
+		controller.RenderIndex(c)
+	})
 
 	// Configura rotas específicas para os recursos (usuários, clientes)
 	UserRoutes(r)
